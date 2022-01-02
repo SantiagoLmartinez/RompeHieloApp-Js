33ecbea3765caccc266130351e1c10b743c1ed49
SantiagoLMartinez <slm17slm17@gmail.com>
2022-01-02 20:30:11 -0300

    mi primer commit

---
+++ js/rhmain.js
@@ -0,0 +1,82 @@
+card01 = document.querySelector("#card01");
+card02 = document.querySelector("#card02");
+card03 = document.querySelector("#card03");
+card04 = document.querySelector("#card04");
+card05 = document.querySelector("#card05");
+card06 = document.querySelector("#card06");
+card07 = document.querySelector("#card07");
+card08 = document.querySelector("#card08");
+card09 = document.querySelector("#card09");
+card10 = document.querySelector("#card10");
+card11 = document.querySelector("#card11");
+card12 = document.querySelector("#card12");
+card13 = document.querySelector("#card13");
+card14 = document.querySelector("#card14");
+card15= document.querySelector("#card15");
+card16 = document.querySelector("#card16");
+card17 = document.querySelector("#card17");
+card18 = document.querySelector("#card18");
+card19 = document.querySelector("#card19");
+card20 = document.querySelector("#card20");
+
+
+function question01(){
+    card01.innerHTML = "¿Gatos o Perros?"
+}
+function question02(){
+    card02.innerHTML = "¿Hubo regalo de navidad?"
+}
+function question03(){
+    card03.innerHTML = "¿Mañana, tarde o noche?"
+}
+function question04(){
+    card04.innerHTML = "¿Tenes alguna foto con alguien famoso?"
+}
+function question05(){
+        card05.innerHTML = "¿Alguna vez te disfrazaste para halloween?"
+}
+function question06(){
+    card06.innerHTML = " ¿Cuál es tu comida favorita?"
+}
+function question07(){
+    card07.innerHTML = "¿Estación favorita del año?"
+}
+function question08(){
+    card08.innerHTML = "¿Ciudad o campo?"
+}
+function question09(){
+    card09.innerHTML = "¿Bailar o cantar?"
+}
+function question10(){
+    card10.innerHTML = "¿Playa o Bosque?"
+}
+function question11(){
+    card11.innerHTML = "¿Bebida favorita?"
+}
+function question12(){
+    card12.innerHTML = "¿Color favorito?"
+}
+function question13(){
+    card13.innerHTML = "¿Que cambiarias de tu pasado?"
+}
+function question14(){
+    card14.innerHTML = "¿Te enojas facilmente?"
+}
+function question15(){
+    card15.innerHTML = "¿Serie favorita?"
+}
+function question16(){
+    card16.innerHTML = "¿Tenes tatuajes?"
+}
+function question17(){
+    card17.innerHTML = "¿Comida favorita?"
+}
+function question18(){
+    card18.innerHTML = "¿Emoji favorito? Describir"
+}
+function question19(){
+    card19.innerHTML = "¿Ensalada de fruta o helado?"
+}
+function question20(){
+    card20.innerHTML = "¿Desayuno o merienda?"
+}
